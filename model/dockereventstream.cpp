4f1db4e98c43a5e2422ef9e6caf9d602eb198186
eduardosaraujo1 <eduardosaraujo1@proton.me>
2026-06-14 17:40:02 -0300

    feat: define behavior of docker event stream

--- model/dockereventstream.cpp
+++ model/dockereventstream.cpp
@@ -2,4 +2,80 @@
 
 DockerEventStream::DockerEventStream(QObject *parent)
     : QObject{parent}
-{}
+{
+    // Connect onStartError to QProcess errorOccured
+
+    // Connect onUnexpectedError to QProcess finished
+
+    // Connect onErrorMessage to QProcess ready stderr output
+
+    // Connect onEventDetected to QProcess ready stdout output
+
+    // Run restart to initiate process
+}
+
+DockerEventStream::~DockerEventStream() {
+    m_attempt_restart = false;
+    stop();
+}
+
+bool DockerEventStream::isActive(){
+    // Check QProcess to see if it's active
+}
+
+void DockerEventStream::onStartError(QProcess::ProcessError error) {
+    // (TODO: move to dedicated function to share with onUnexpectedError)
+    // Increment the fail counter (if it's less than 0 then set it to 1)
+    // If it's less than or equal to 3 and attempt_restart is true, try calling .restart() and log the attempt
+    // Else set attempt_restart to false, log and emit criticalStreamError
+}
+
+void DockerEventStream::onUnexpectedError(int exitCode, QProcess::ExitStatus exitStatus) {
+    // (TODO: move to dedicated function to share with onStartError)
+    // Increment the fail counter (if it's less than 0 then set it to 1)
+    // If it's less than or equal to 3 and attempt_restart is true, try calling .restart() and log the attempt
+    // Else set attempt_restart to false, log and emit criticalStreamError
+}
+
+void DockerEventStream::onErrorMessage() {
+    // Read std error
+
+    // Log error
+
+    // Won't attempt to restart anything
+}
+
+void DockerEventStream::onEventDetected() {
+    // Read standard output
+
+    // Parse JSON
+
+    // If it fails (always log) and consecutive error count is greater than 3, set attempt_restart to false and emit critical error
+    // Otherwise, ignore missed event and early return.
+
+    // If success, reset error count (if a parse was successful we're sure it's healthy) and store parsed value
+
+    // Emit eventReceived with parsed value
+}
+
+void DockerEventStream::stop() {
+    m_attempt_restart = false;
+
+    if (proc.state() == QProcess::Running) {
+        proc.terminate();
+
+        if (! proc.waitForFinished(3000)) {
+            proc.kill();
+        }
+    }
+}
+
+void DockerEventStream::restart() {
+    // Stop process if it's running
+
+    // Set keepAlive to true
+
+    // Run proper docker events command
+
+    // // Triggers auto restart behavior, check onUnexpectedError and onStartError
+}
--- model/dockereventstream.h
+++ model/dockereventstream.h
@@ -2,15 +2,38 @@
 #define DOCKEREVENTSTREAM_H
 
 #include <QObject>
+#include <QProcess>
 #include <model/data-objects/dockerevent.h>
 
 class DockerEventStream : public QObject
 {
     Q_OBJECT
+private:
+    int m_consecutive_errors = 0;
+    bool m_attempt_restart = true;
+    QProcess proc{this};
 public:
     explicit DockerEventStream(QObject *parent = nullptr);
+
+    ~DockerEventStream();
+
+    bool isActive();
+private slots:
+    void onStartError(QProcess::ProcessError error);
+
+    void onUnexpectedError(int exitCode, QProcess::ExitStatus exitStatus);
+
+    void onErrorMessage();
+
+    void onEventDetected();
+
+    void stop();
+
+    void restart();
 signals:
     void eventReceived(const DockerEvent event);
+
+    void criticalError();
 };
 
 #endif // DOCKEREVENTSTREAM_H
