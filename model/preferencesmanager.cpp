bba4f7f38346b43dc2954d9d76b0ed25c09a47bf
eduardosaraujo1 <eduardosaraujo1@proton.me>
2026-06-15 16:52:22 -0300

    feat: write spec for prefs manager

--- model/preferencesmanager.cpp
+++ model/preferencesmanager.cpp
@@ -2,4 +2,8 @@
 
 PreferencesManager::PreferencesManager(QObject *parent)
     : QObject{parent}
-{}
+{
+    m_config_relpath = QStandardPaths::writableLocation(
+            QStandardPaths::AppConfigLocation)
+        + "/simple-container-manager/preferences.yaml";
+}
--- model/preferencesmanager.h
+++ model/preferencesmanager.h
@@ -2,6 +2,13 @@
 #define PREFERENCESMANAGER_H
 
 #include <QObject>
+#include <QList>
+#include <QSet>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QStandardPaths>
+#include <QFile>
 
 /*
  * Needs attribute to specify config file path on Windows, Linux and MacOS
@@ -12,9 +19,43 @@ class PreferencesManager : public QObject
 {
     Q_OBJECT
 public:
+    struct ContainerSpec {
+        QString name; // required
+        QString label; // required
+        QString icon; // nullable
+        QString action; // nullable
+    };
+
+private:
+    QList<ContainerSpec> m_containers;
+    QSet<QString> m_container_names;
+    QString m_config_relpath; // Don't forget to set this in the constructor
+public:
     explicit PreferencesManager(QObject *parent = nullptr);
 
+    ~PreferencesManager();
+
+    const QList<ContainerSpec>& containers() const {
+        return this->m_containers;
+    };
+
+    const QSet<QString>& names() const {
+        return this->m_container_names;
+    };
+public slots:
+    void refreshConfig();
+private:
+    void writeDefaultConfigs(QFile &file) const;
+
+    QByteArray readFileData(QFile &file) const;
+
+    // std::optional<YamlDocument> parseConfig(const QByteArray) const;
+
+    // Figure out how YamlDocument will represent arrays (`containers:`) and write the spec
 signals:
+    void preferencesUpdated();
+
+    void criticalError();
 };
 
 #endif // PREFERENCESMANAGER_H
