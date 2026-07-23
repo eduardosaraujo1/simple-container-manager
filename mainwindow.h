d1b544333d082b89317aaabc934dab938c415540
eduardosaraujo1 <eduardosaraujo1@proton.me>
2026-07-22 22:17:24 -0300

    docs: small arch changes

--- mainwindow.h
+++ mainwindow.h
@@ -10,11 +10,10 @@ class MainWindow;
 QT_END_NAMESPACE
 
 /*
-First of all, BEFORE implementing the class below, answer this prompt with which files you need in order to have enough
-context to implement everything without writing incorrect code.
-
-MainWindow either instantiates or receives the data sources in the constructor (not both though; I'll leave this decision to the whims of the LLM)
+MainWindow either instantiates or receives the data sources in the constructor (not both though; I'll leave this
+decision to the whims of the LLM)
 Sources: <data/dockercli.h> <data/dockereventstream.cpp> <data/apppreferences.h>
+Sources: <data/data-objects/containerinfo.h>
 MainWindow needs to find a way to, cleanly and as soon as it is loaded:
 - Hook ui->btnExit into an application exit call
 - Start app preferences (call read config file at least once and don't mutate ui->containerList until it's loaded)
@@ -26,12 +25,12 @@ MainWindow needs to find a way to, cleanly and as soon as it is loaded:
 - Ensure DockerEventStream is up (call .start() after initialization)
 - Hook ui->btnRefresh click into the container state reload logic (more on that later)
 - Hook the signal DockerEventStream::eventReceived into the reload logic (more on that later)
-    - DockerEventStream::criticalError exists. If it fails the application isn't exactly broken, it just doesn't refresh automatically, so I'm unsure what to do. Make a little text in the corner?
+    - DockerEventStream::criticalError exists. If it fails the application isn't exactly broken, it just doesn't refresh automatically, so I'm unsure what to do. Add a little warning at the corner maybe (add a stub and comment whatever you think is best)
 - Hook ui->containerList.containerToggle(const QString &containerName, bool isStartCommand) into a DockerCLI startContainer or stopContainer call.
     - To comply with optimistic UI requirements, we should show the container as started before DockerCLI responds with the new state. Currently the ContainerListWidget (ui->containerList) does not have a method to do that. Pretend it does and write a comment above it, signaling that I should implement that.
 - Hook ui->containerList.containerAction(const QString &containerName, const QString &action) into a new QProcess to run (mark a TODO that says soon a refactor will abstract this into a domain ActionRunner service/usecase class)
     - Disable the button and reenable it after 1 second, I want to avoid process spamming
-- Implement the reload logic: 
+- Implement the reload logic in a dedicated domain/reloadcontainersaction.cpp file: 
     - Disable the refresh button
     - Request a container refresh through DockerCLI::requestContainerRefresh
     - Wait on signal DockerCLi::containersUpdated:
