92625e3369cba47dccf22f8acb5bb1dcab71fc4c
eduardosaraujo1 <eduardosaraujo1@proton.me>
2026-07-29 16:53:08 -0300

    feat(partial): progress on container read logic

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QObject>
+
+#include <data/appconfigreader.h>
+#include <data/dockereventstream.h>
+#include <data/dockercli.h>
+#include <domain/containerservice.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setStyle("windowsvista");
+
+    AppConfigReader configReader;
+
+    QObject::connect(&configReader, &AppConfigReader::errorOccurred, [](AppConfigReader::Error error) {
+        // Handle cases for write error (not warning) and read error (critical)
+    });
+
+    // TODO: handle success path on configReader
+
+    configReader.readConfigFile();
 
     MainWindow w;
     w.show();
